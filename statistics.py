598123e24bc304096a0aa686c2a4c200e3ffe762
Prithvi Makam Lokanath <105284293+MLPrithvi@users.noreply.github.com>
2022-10-19 16:18:21 +0530

    Update statistics.py

--- statistics.py
+++ statistics.py
@@ -4,8 +4,8 @@ def calculateStats(numbers):
   length = len(numbers)
   if length == 0:
     return None
-  test = {"avg":avgVal, "min":minVal, "max":maxVal} 
   avgVal = sum(numbers) / length
   minVal = min(numbers)
   maxVal = max(numbers)
+  test = {"avg":avgVal, "min":minVal, "max":maxVal} 
   return test
