349dfb4521fe89c3054ba16c1758be6b74660c78
Prithvi Makam Lokanath <105284293+MLPrithvi@users.noreply.github.com>
2022-10-19 17:06:33 +0530

    Update statistics.py

--- statistics.py
+++ statistics.py
@@ -3,9 +3,13 @@ import math as m
 def calculateStats(numbers):
   length = len(numbers)
   if length == 0:
-    return m.isnan(numbers)
+    a = m.isnan(sum(numbers) / length)
+    b = m.isnan(min(numbers))
+    c = m.isnan(max(numbers))
+    test2 = {"avg":a, "min":b, "max":c}
+    return test2
   avgVal = sum(numbers) / length
   minVal = min(numbers)
   maxVal = max(numbers)
-  test = {"avg":avgVal, "min":minVal, "max":maxVal}
-  return test
+  test1 = {"avg":avgVal, "min":minVal, "max":maxVal}
+  return test1
