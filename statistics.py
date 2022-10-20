734f33ffecbc80fc43f473b676e61fa9127b8672
Prithvi Makam Lokanath <105284293+MLPrithvi@users.noreply.github.com>
2022-10-20 11:19:08 +0530

    Update statistics.py

--- statistics.py
+++ statistics.py
@@ -4,10 +4,7 @@ def calculateStats(numbers):
   if type(numbers) == list:
     length = len(numbers)
     for x in range(length):
-      if type(numbers[x]) == str:
-        Nan_Output = m.isnan(x)
-        return Nan_Output
-      elif (type(numbers[x]) == float) or (type(numbers[x]) == int) and (length != 0):
+      if (type(numbers[x]) == float) or (type(numbers[x]) == int) and (length != 0):
         avgVal = sum(numbers) / length
         maxVal = max(numbers)
         minVal = min(numbers)
